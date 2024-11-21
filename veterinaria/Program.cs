a56cbe3243354058dc753845d8e407b1f0139cc7
Akronixx <josectp12@gmail.com>
2024-11-21 10:16:40 -0500

    crear

--- veterinaria/Program.cs
+++ veterinaria/Program.cs
@@ -1,9 +1,9 @@
 ﻿/*
 cree una aplicativo de veterinaria
 clases con propiedades: 
-    Mascota: nombre, edad, especie
-    Servicio: descripcion, precio
-    Boleta: codigo, mascota, servicio1, servicio2, total
+   Mascota: nombre, edad, especie
+   Servicio: descripcion, precio
+   Boleta: codigo, mascota, servicio1, servicio2, total
 
 Use arreglos estáticos de las clases
 Debe tener un menu con estas opciones:
@@ -17,4 +17,62 @@ sus respectivos arreglos
 Divida entre sus integrantes, el lider consolida y los integrantes uno hace lo de servicios y el otro el de mascotas
 Debe tener una rama por integrante, el lider debe tener por lo menos 2 ramas: main y dev. debe trabajar y consolidar en dev
 */
+using Veterinario;
+creard d = new creard();
+byte opcion;
 
+string regresar;
+
+
+do
+{
+
+    Console.WriteLine("Programa de Desayunos\n");
+
+    Console.WriteLine("******* MENÚ *******");
+
+    Console.WriteLine("* 1. Crear         *");
+    Console.WriteLine("* 2. Lista         *");
+
+    Console.WriteLine("* 3. Eliminar      *");
+
+    Console.WriteLine("* 0. Salir         *");
+
+    Console.WriteLine("********************\n");
+
+
+
+    Console.Write("Ingrese opcion: ");
+
+
+
+    while (!byte.TryParse(Console.ReadLine(), out opcion) || opcion > 3)
+    {
+
+        Console.Write("Error: Ingrese opcion: ");
+
+    }
+
+
+
+    switch (opcion)
+    {
+
+        case 0: Environment.Exit(0); break;
+
+        case 1: d.crear(); break;
+
+        case 2: break;
+
+        case 3: break;
+
+
+    }
+
+    Console.WriteLine("Desea regresar al menú? [si]: ");
+
+    regresar = Console.ReadLine();
+
+    Console.Clear();
+
+} while (regresar == "si");
---
+++ veterinaria/mascota.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinario
+{
+    public class creard
+    {
+        string[] lista1 = new string[0];
+        int[] lista2 = new int[0];
+        string[] lista3 = new string[0];
+        string[] lista4 = new string[0];
+        string[] lista5 = new string[0];
+
+        int posicion = 0;
+
+        public void crear()
+        {
+            string RespuestaN;
+            Console.Write("Ingresa el nombre de animal : ");
+            RespuestaN = Console.ReadLine();
+            Array.Resize(ref lista1, lista1.Length + 1);
+            lista1[posicion] = RespuestaN;
+            posicion = posicion + 1;
+            Console.WriteLine("A agregado un nombre ");
+            int RespuestaEd;
+            Console.Write("Ingresa la edad del animal : ");
+            RespuestaEd = Convert.ToInt32(Console.ReadLine());
+            Array.Resize(ref lista2, lista2.Length + 1);
+            lista2[posicion] = RespuestaEd;
+            posicion = posicion + 1;
+            Console.WriteLine("A agregado la edad del animal ");
+            string RespuestaEs;
+            Console.Write("Ingresa el nombre del perro : ");
+            RespuestaEs = Console.ReadLine();
+            Array.Resize(ref lista3, lista3.Length + 1);
+            lista3[posicion] = RespuestaEs;
+            posicion = posicion + 1;
+            Console.WriteLine("A agregado la especie del animal ");
+            string servicio1;
+            Console.Write("Ingresa el nombre de animal : ");
+            servicio1 = Console.ReadLine();
+            Array.Resize(ref lista4, lista4.Length + 1);
+            lista4[posicion] = servicio1;
+            string servicio2;
+            Console.Write("Ingresa el nombre de animal : ");
+            servicio2 = Console.ReadLine();
+            Array.Resize(ref lista5, lista5.Length + 1);
+            lista5[posicion] = servicio2;
+        }
+    }
+}
